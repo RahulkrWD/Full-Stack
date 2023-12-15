0ff414f69c31e09b7ff8b15d8c2e6e685c2a99d1
rahul-tkt <rahulkmrgaya21@gmail.com>
2023-12-15 20:53:39 +0530

    html, css and javascript

---
+++ project_1/script.js
@@ -0,0 +1,67 @@
+// Typing Animation
+function typingEffect() {
+  const contactTexts = shuffleArray([
+    "HTML",
+    "CSS",
+    "javascript",
+    "Bootstrap",
+    "ReactJs",
+  ]);
+  const typedtext = document.getElementsByClassName("typedtext")[0];
+  let removing = false;
+  let idx = (char = 0);
+  setInterval(() => {
+    if (char < contactTexts[idx].length)
+      typedtext.innerHTML += contactTexts[idx][char];
+    if (char == contactTexts[idx].length + 15) removing = true;
+    if (removing)
+      typedtext.innerHTML = typedtext.innerHTML.substring(
+        0,
+        typedtext.innerHTML.length - 1
+      );
+    char++;
+    if (typedtext.innerHTML.length === 0) {
+      if (idx === contactTexts.length - 1) idx = 0;
+      else idx++;
+      char = 0;
+      removing = false;
+    }
+  }, 100);
+}
+typingEffect();
+function shuffleArray(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// counter
+let counterValue = 0;
+setInterval(function () {
+  if (counterValue <= 10000) {
+    counterValue++;
+    document.getElementById("counter1").innerText = counterValue;
+  }
+});
+
+setInterval(function () {
+  if (counterValue < 15000) {
+    counterValue++;
+    document.getElementById("counter2").innerText = counterValue;
+  }
+});
+setInterval(function () {
+  if (counterValue < 8000) {
+    counterValue++;
+    document.getElementById("counter3").innerText = counterValue;
+  }
+});
